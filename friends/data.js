f21c00ba9c83abf83009f9412a6fb818f805ff70
KazIDK <discordcopier@gmail.com>
2026-03-05 11:02:58 +0300

    Update data.js

--- friends/data.js
+++ friends/data.js
@@ -8,6 +8,13 @@ window.FRIENDS = [
     tagline: "bro is built different",
     desc: "Balls",
     image: "/assets/friends/mj//mjoodi.jpg"
-  }
+  },
+  {
+    id: "MORE PPL",
+    name: "ADDING SOON ITS JS SO MUCH",
+    tagline: "67",
+    desc: "420",
+    image: "/assets/friends/mj//mjoodi.jpg"
+  },
 ];
 
