410c0fc16e935c1a55f8cb3ad815ece0980139a1
KazIDK <discordcopier@gmail.com>
2026-03-05 11:01:20 +0300

    s

---
+++ friends/data.js
@@ -0,0 +1,13 @@
+// friends/data.js
+// Add / edit friends here only.
+
+window.FRIENDS = [
+  {
+    id: "mjood",
+    name: "Mjood",
+    tagline: "bro is built different",
+    desc: "Balls",
+    image: "/assets/friends/mj//mjoodi.jpg"
+  }
+];
+
